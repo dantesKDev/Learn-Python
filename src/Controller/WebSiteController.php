3f70ede91c9c18aa50a6a6aaf925226175cef7ef
dantesK <david.mona.mpro@gmail.com>
2019-01-29 18:52:32 +0100

    Troisième commit

--- src/Controller/WebSiteController.php
+++ src/Controller/WebSiteController.php
@@ -28,13 +28,32 @@ class WebSiteController extends AbstractController
     }
 
     /**
-     * @Route("/Debutant/", name="debut")
+     * @Route("/Initiation/{id}", name="initiation")
      */
-    public function index(CategorieGenerator $categorieGenerator)
+    public function debutAction($id)
     {
-
+      $parent_id = $id;
       $repo = $this->getDoctrine()->getRepository(Categorie::class);
-      $categories = $repo->findAll();
+      $categories = $repo->find($parent_id);
+      // $categories = $categorieGenerator->getCategorieList();
+
+      $params = array(
+        'categories' => $categories,
+        'controller_name' => 'WebSiteController'
+      );
+      return $this->render('web_site/init.html.twig', $params);
+
+    }
+
+
+    /**
+     * @Route("/Perfectionnement/{id}", name="perfect")
+     */
+    public function debutAction($id)
+    {
+      $parent_id = $id;
+      $repo = $this->getDoctrine()->getRepository(Categorie::class);
+      $categories = $repo->find($parent_id);
       // $categories = $categorieGenerator->getCategorieList();
 
       $params = array(
@@ -49,37 +68,37 @@ class WebSiteController extends AbstractController
     /**
      * @Route("/Intermediaire/", name="inter")
      */
-    public function index(CategorieGenerator $categorieGenerator)
-    {
-
-      $repo = $this->getDoctrine()->getRepository(Categorie::class);
-      $categories = $repo->findAll();
-      // $categories = $categorieGenerator->getCategorieList();
-
-      $params = array(
-        'categories' => $categories,
-        'controller_name' => 'WebSiteController'
-      );
-      return $this->render('web_site/index.html.twig', $params);
-
-    }
+    // public function index(CategorieGenerator $categorieGenerator)
+    // {
+    //
+    //   $repo = $this->getDoctrine()->getRepository(Categorie::class);
+    //   $categories = $repo->findAll();
+    //   // $categories = $categorieGenerator->getCategorieList();
+    //
+    //   $params = array(
+    //     'categories' => $categories,
+    //     'controller_name' => 'WebSiteController'
+    //   );
+    //   return $this->render('web_site/index.html.twig', $params);
+    //
+    // }
 
 
     /**
      * @Route("/Avance/", name="Avan")
      */
-    public function index(CategorieGenerator $categorieGenerator)
-    {
-
-      $repo = $this->getDoctrine()->getRepository(Categorie::class);
-      $categories = $repo->findAll();
-      // $categories = $categorieGenerator->getCategorieList();
-
-      $params = array(
-        'categories' => $categories,
-        'controller_name' => 'WebSiteController'
-      );
-      return $this->render('web_site/index.html.twig', $params);
-
-    }
+    // public function index(CategorieGenerator $categorieGenerator)
+    // {
+    //
+    //   $repo = $this->getDoctrine()->getRepository(Categorie::class);
+    //   $categories = $repo->findAll();
+    //   // $categories = $categorieGenerator->getCategorieList();
+    //
+    //   $params = array(
+    //     'categories' => $categories,
+    //     'controller_name' => 'WebSiteController'
+    //   );
+    //   return $this->render('web_site/index.html.twig', $params);
+    //
+    // }
 }
